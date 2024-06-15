789e6e9ea20aa275a47033408d14659382ad75cb
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-06-15 14:26:18 -0500

    Public routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,22 +3,28 @@ import { Routes, Route } from 'react-router-dom';
 
 import { HeroesRoutes } from "../heroes";
 import { LoginPage } from '../auth';
-import { PrivateRoute } from './PrivateRoute';
+import { PrivateRoute, PublicRoute } from './';
+
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-
-        <Route path="login" element={ <LoginPage /> } />
+        <Route path="login/*" element={
+            <PublicRoute>
+              <Route path="/*" element={ <LoginPage /> } />
+            </PublicRoute>
+          }
+        />
 
         <Route path="/*" element={
-          <PrivateRoute>
-            <HeroesRoutes />
-          </PrivateRoute>
-        } />
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          }
+        />
+        {/* <Route path="login" element={ <LoginPage /> } /> */}
         {/* <Route path="/*" element={ <HeroesRoutes /> } /> */}
-        
       </Routes>
     </>
   );
